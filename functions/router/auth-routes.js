9593e3c0bcc14ac735597dcc47ea1997bf8460d4
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2020-08-11 14:14:43 +0800

    register fix

--- functions/router/auth-routes.js
+++ functions/router/auth-routes.js
@@ -66,7 +66,6 @@ router.post('/register', async (req, res) => {
       let user = {
         username: req.body.username,
         password: hashedPassword,
-        googleID: req.body.googleID,
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
@@ -77,7 +76,7 @@ router.post('/register', async (req, res) => {
       const result = await usersRef.add(user, { merge: true });
 
       console.log('Added user with username and password: ', result.id);
-      return res.status(200).json({ successful: true });
+      return res.status(200).json({ successful: true, url: '/auth' });
     } else {
       console.log('user exists.');
       // user exists
@@ -93,68 +92,13 @@ router.post('/register', async (req, res) => {
         });
 
         console.log('Updated user with username and password: ', result.id);
-        return res.status(200).json({ successful: true });
+        return res.status(200).json({ successful: true, url: '/auth' });
       } else {
         return res
           .status(500)
           .json({ successful: false, message: 'user exists' });
       }
     }
-    // db.collection('users')
-    //   .where('email', '==', req.body.email)
-    //   .get()
-    //   .then((snapshot) => {
-    //     // check if user exists
-    //     if (snapshot.empty) {
-    //       // add new user
-    //       console.log('user exists not.');
-
-    //       let user = {
-    //         username: req.body.username,
-    //         password: hashedPassword,
-    //         googleID: req.body.googleID,
-    //         firstName: req.body.firstName,
-    //         lastName: req.body.lastName,
-    //         email: req.body.email,
-    //         picUrl: req.body.picUrl,
-    //       };
-    //       console.log(user);
-
-    //       db.collection('users')
-    //         .add(user, { merge: true })
-    //         .then((data) => {
-    //           console.log('new user created:');
-    //           return res.status(200).json({ successful: true });
-    //         })
-    //         .catch((err) => {
-    //           return res.status(500).json({ successful: false });
-    //         });
-    //     } else {
-    //       console.log('user exists.');
-    //       // user exists
-    //       // check if user already completed registration
-
-    //       const user = snapshot.docs[0].data();
-    //       if (user.username === '' && user.password === '') {
-    //         db.collection('users')
-    //           .doc(snapshot.docs[0].id)
-    //           .update({
-    //             username: req.body.username,
-    //             password: hashedPassword,
-    //           })
-    //           .then((data) => {
-    //             console.log('updated username and password:');
-    //             return res.status(200).json({ successful: true });
-    //           })
-    //           .catch((err) => {
-    //             return res.status(500).json({ successful: false });
-    //           });
-    //       }
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     return res.status(500).json({ successful: false });
-    //   });
   } catch (err) {
     console.log(err);
 
@@ -172,31 +116,47 @@ router.get(
 router.get(
   '/google/redirect',
   passport.authenticate('google', { session: false }),
-  (req, res) => {
-    const user = {
-      id: req.user.id,
-      email: req.user.email,
-    };
-
-    // calc expire date
-    let expiresIn = new Date();
-    expiresIn.setSeconds(expiresIn.getSeconds() + addedSeconds);
-
-    const token = jwt.sign({ user }, keys.jwt.secretKey, {
-      expiresIn: addedSeconds,
-    });
-
+  async (req, res) => {
     // return res.json({ token, userId: user.id, expires: expiresIn.toISOString });
-    res.redirect(
-      url.format({
-        pathname: keys.url.front_end + '/auth',
-        query: {
-          token,
-          userId: user.id,
-          expires: expiresIn.toISOString(),
-        },
-      })
-    );
+    try {
+      const user = req.user;
+      console.log('redirect: ', user, user.username === '');
+
+      if (user.username === '') {
+        res.redirect(
+          url.format({
+            pathname: keys.url.front_end + '/register',
+            query: user,
+          })
+        );
+      } else {
+        const user = {
+          id: req.user.id,
+          email: req.user.email,
+        };
+        // calc expire date
+        let expiresIn = new Date();
+        expiresIn.setSeconds(expiresIn.getSeconds() + addedSeconds);
+
+        const token = jwt.sign({ user }, keys.jwt.secretKey, {
+          expiresIn: addedSeconds,
+        });
+
+        res.redirect(
+          url.format({
+            pathname: keys.url.front_end + '/auth',
+            query: {
+              token,
+              userId: user.id,
+              expires: expiresIn.toISOString(),
+            },
+          })
+        );
+      }
+    } catch (err) {
+      console.log(err);
+      res.json({ successful: false });
+    }
   }
 );
 
